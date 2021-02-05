01dc30c4e5382e0b16e79ccc0365d416d8a59816
Vitalii Kravchyshyn <vitalii.kravchyshyn@paybee.io>
2021-02-05 17:08:14 +0200

    create list of courses

--- routes/courses.js
+++ routes/courses.js
@@ -1,10 +1,15 @@
 const { Router } = require('express');
 const router = Router();
+const Course = require('../models/course')
 
-router.get('/', (request, response) => {
+router.get('/', async (request, response) => {
+
+    const courses = await Course.getAll();
+
     response.render('courses', {
         title: 'Courses',
-        isCourses: true
+        isCourses: true,
+        courses
     });
 })
 
