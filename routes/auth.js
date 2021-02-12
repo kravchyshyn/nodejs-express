38feb0cd08b10206f0a07f650f32f7b67973eb32
Vitalii Kravchyshyn <vitalii.kravchyshyn@paybee.io>
2021-02-12 02:23:32 +0200

    implement registration method, route

--- routes/auth.js
+++ routes/auth.js
@@ -30,4 +30,26 @@ router.post('/login', async (req, res) => {
     });
 });
 
+router.post('/register', async (req, res) => {
+    try {
+        const {name, email, password, confirm } = req.body;
+        const candidate = await User.findOne({email});
+
+        if (candidate) {
+            res.redirect('/auth/login#register');
+        } else {
+            const user = new User(
+                { name, email, password, cart: { items: []} 
+            });
+
+            await user.save();
+            res.redirect('/auth/login#login');
+    
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+});
+
 module.exports = router;
